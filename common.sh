86470c62fb17dc31c773bfd2f5f1807f8c2a6eaf
yx <yxing.xyz@gmail.com>
2023-03-24 01:44:11 +0800

    fix：修补bug

--- common.sh
+++ common.sh
@@ -110,8 +110,8 @@ EOF
 
     ## terminal
     emerge -u app-containers/docker-cli app-shells/zsh app-misc/neofetch app-misc/trash-cli \
-        app-shells/fzf app-text/tree dev-db/mycli dev-vcs/lazygit dev-util/git-delta sys-apps/bat \
-        sys-apps/fd sys-apps/sd sys-apps/lsd sys-process/lsof sys-apps/ripgrep sys-process/htop sys-process/iotop \
-        strace cloc dev-util/shellcheck-bin app-admin/helm exa sshfs cmus app-misc/jq diff-so-fancy caddy \
-        www-apps/hugo v2ray-bin ntp rustupzoxide
+           app-shells/fzf app-text/tree dev-db/mycli dev-vcs/lazygit dev-util/git-delta sys-apps/bat \
+           sys-apps/fd sys-apps/sd sys-apps/lsd sys-process/lsof sys-apps/ripgrep sys-process/htop sys-process/iotop \
+           strace cloc dev-util/shellcheck-bin app-admin/helm exa sshfs cmus app-misc/jq diff-so-fancy caddy \
+           www-apps/hugo v2ray-bin ntp rustup zoxide
 }
