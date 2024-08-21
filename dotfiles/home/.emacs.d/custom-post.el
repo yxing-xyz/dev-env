d3ac7be6a264a82338d6a0a235e53e4e4ace067f
Ethan <yxing.xyz@gmail.com>
2024-08-21 15:57:50 +0800

    sync from centaur

--- dotfiles/home/.emacs.d/custom-post.el
+++ dotfiles/home/.emacs.d/custom-post.el
@@ -7,9 +7,9 @@
 (setq blink-cursor-mode nil)
 ;; 光标实心
 (setq-default cursor-type 'box)
-
 ;; 设置mode-line显示路径风格
 (setq doom-modeline-buffer-file-name-style 'relative-from-project)
+
 ;; key重定向
 ;; (define-key key-translation-map (kbd "M-\\") (kbd "M-g w"))
 ;; (define-key prog-mode-map (kbd "M-RET") (key-binding (kbd "M-g w")))
@@ -17,8 +17,7 @@
                                              (local-set-key (kbd "M-RET") (key-binding (kbd "C-:")))
                                              )))
 (add-hook 'prog-mode-hook (lambda () (progn
-                                  (local-set-key (kbd "C-c @ `") 'hideshow-hydra/body)
-                                  )))
-
+                                       (local-set-key (kbd "C-c @ `") 'hideshow-hydra/body)
+                                       )))
 ;; 恢复会话
 (desktop-read)
